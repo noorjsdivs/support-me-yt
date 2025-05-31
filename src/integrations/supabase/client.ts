21f576c8f2911c6ffea888ae7ea84f12ab9fce5f
noorjsdivs <noor.jsdivs@gmail.com>
2025-05-31 21:06:36 +0600

    .env file added

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,9 @@
 import { createClient } from "@supabase/supabase-js";
 import type { Database } from "./types";
 
-const SUPABASE_URL = "";
-const SUPABASE_PUBLISHABLE_KEY = "";
+const SUPABASE_URL = "https://nxnejhskwieaclwtjbnf.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im54bmVqaHNrd2llYWNsd3RqYm5mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcyMTA2ODgsImV4cCI6MjA2Mjc4NjY4OH0.FpeHl_p4AL8QbfmeBQLJWjgmHexpXU0A9yYvTsT4wds";
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
